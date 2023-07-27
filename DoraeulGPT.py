609ab349833fbc12876029ce768ece0dc3498fba
Doraeul <121475032+yourselfvv@users.noreply.github.com>
2023-07-27 22:48:27 +0900

    Add files via upload

--- DoraeulGPT.py
+++ DoraeulGPT.py
@@ -1,52 +1,51 @@
-def add_to_tree(tree, words, answer):
-    if not words:
-        return
+import random
 
-    word = words.pop(0)
+def most(n: int, str_list: list):
+    result = []
+    result_num = []
+    for i in str_list:
+        result.append([i, str_list.count(i)])
+    for p in result:
+        result_num.append(int(p[1]))
+    num_list = sorted(result_num, reverse=True)
+    if not num_list:
+        return None
+    if n >= len(num_list):
+        n = len(num_list) - 1
 
-    if word in tree:
-        add_to_tree(tree[word], words.copy(), answer)
-    else:
-        if words:
-            tree[word] = {}
-            add_to_tree(tree[word], words.copy(), answer)
-        else:
-            tree[word] = {"answer": answer}
+    return result[result_num.index(num_list[n])]
 
-
-def get_all_values(tree, prefix=None):
-    values = []
-    for key, value in tree.items():
-        if isinstance(value, dict):
-            if prefix:
-                values.extend(get_all_values(value, f"{prefix} {key}"))
-            else:
-                values.extend(get_all_values(value, key))
-        elif key == "answer":
-            if prefix:
-                values.append({prefix: value})
-            else:
-                values.append({key: value})
-    return values
-
-
-def main():
-    questions = []
-    answers = []
-    tree = {}
+def train(times:int):
+    f = open('qwetyuio.txt', 'r', encoding='UTF-8')
+    f.close()
     f = open('data.txt', 'r', encoding='UTF-8')
-
-    for i in f:
-        questions.append(i[:i.find(',')])
-        answers.append(i[i.find(',') + 1:-3].strip())
-
-    for question, answer in zip(questions, answers):
-        words = question.split()
-        add_to_tree(tree, words, answer)
-
-    all_values = get_all_values(tree)
-    print(tree)
+    question = [i for i in f]
+    question_answer = []
+    for _ in range(times):
+        i = random.choice(question)
+        for j in i[:i.find(',')].split():
+            for k in i[i.find(',')+1:-3].split():
+                question_answer.append([j, k])
+    return question_answer
 
 
-if __name__ == "__main__":
-    main()
+answer_list = train(10000)
+for _ in range(15):
+    question = input('>>> ').split()
+    answer = []
+    for g in question:
+        x = []
+        for l in answer_list:
+            if l[0] == g:
+                x.append(l[1])
+        answer.append(most(n=random.randint(0, len(x)), str_list=x))
+    answer_main = ""
+    answer_last = ""
+    for q in answer:
+        if '.' in q:
+            answer_last += q[0]
+            answer_last += " "
+            continue
+        answer_main += q[0]
+        answer_main += " "
+    print(answer_main+answer_last)
